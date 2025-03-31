b9aac17dea5ac6b471437a99d3180d84ba712b09
DanielZhuZhang <danielzhuzhang@gmail.com>
2025-03-31 19:05:52 -0400

    v1

---
+++ src/Components/title.tsx
@@ -0,0 +1,8 @@
+const TitlePage = () => {
+    return (
+        <h1>CS3733-D25 Team 19: Pottery and Cello</h1>
+    );
+};
+
+// Export the component so it can be used in other files
+export default TitlePage;
