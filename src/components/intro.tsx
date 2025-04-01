506306d373f3aa757e47c4ff949e1fddd72fe514
jdu3 <jdu+githubCS3733@anonyrat.com>
2025-03-31 20:55:18 -0400

    title and welcome initial version

---
+++ src/components/intro.tsx
@@ -0,0 +1,14 @@
+/*const IntroPage = () => {
+
+};
+*/
+
+
+
+const Welcome = ({name1, name2}:{name1: string, name2: string}) => {
+    return (
+        <p>Names: {name1} and {name2}</p>
+    );
+}
+
+export default Welcome;
